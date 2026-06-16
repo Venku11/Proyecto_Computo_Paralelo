2966ffeeb91000da4def229a822113ccb93dd3ad
amme140905-afk <amme140905@gmail.com>
2026-06-15 20:55:29 -0600

    Create metricas_paralelas.py

---
+++ metricas_paralelas.py
@@ -0,0 +1,33 @@
+def calcular_metricas_paralelas(procesos, tiempos):
+
+    resultados = []
+    tiempo_base = tiempos[0]
+
+    for i in range(len(procesos)):
+
+        p = procesos[i]
+        tiempo_p = tiempos[i]
+
+        speedup = tiempo_base / tiempo_p
+        eficiencia = speedup / p
+
+        if p == 1:
+            karp_flatt = 0
+            amdahl = 1
+            gustafson = 1
+        else:
+            karp_flatt = ((1 / speedup) - (1 / p)) / (1 - (1 / p))
+            amdahl = 1 / (karp_flatt + ((1 - karp_flatt) / p))
+            gustafson = p - karp_flatt * (p - 1)
+
+        resultados.append({
+            "procesos": p,
+            "tiempo": tiempo_p,
+            "speedup": speedup,
+            "eficiencia": eficiencia,
+            "amdahl": amdahl,
+            "gustafson": gustafson,
+            "karp_flatt": karp_flatt
+        })
+
+    return resultados
